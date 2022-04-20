ffeaa6e68d6570a9cd62bded14f2819f07284bdc
Gopalkrishna9905 <88495457+Gopalkrishna9905@users.noreply.github.com>
2022-04-20 11:45:56 +0530

    Add files via upload

---
+++ LinearSearch.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+  bool LinSearch(int arr[],int size,int key)
+  {
+  	for(int i =0;i<size;i++)
+  	{
+  		if(arr[i]==key)
+  		{
+  			return 1;
+		  }
+	  }
+	  return 03;
+  }
+
+int main()
+{
+	int arr[5]={1,2,3,4,5};
+	int key;
+	cout<<" enter key to find : ";
+	cin>>key;
+	bool found = LinSearch(arr,5,key);
+	if(found)
+	{
+		cout<<" key is present " <<endl;
+	}
+	else{
+		cout<<"key is absent";
+	}
+	return 0;
+	
+}
